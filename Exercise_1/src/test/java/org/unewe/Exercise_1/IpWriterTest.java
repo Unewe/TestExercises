a6320eda0fe640486f6a8c713e6d2cfad933d6dc
Marsianca <Marsianca@192.168.1.198>
2018-09-11 17:50:44 +0300

    IpWriterTest changes

--- Exercise_1/src/test/java/org/unewe/Exercise_1/IpWriterTest.java
+++ Exercise_1/src/test/java/org/unewe/Exercise_1/IpWriterTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class IpWriterTest {
+public class IpWriterTest extends junit.framework.Assert {
 
 	@Test
 	public void testWrite() {
-		assertEquals(new ArrayList<String>(){{add("192.168.0.4");add("192.168.0.3");add("192.168.0.2");}}, IpWriter.write(new String[] {"192.168.0.1", "192.168.0.5"}));
+		assertEquals(new ArrayList<String>(){{add("192.168.0.2");add("192.168.0.3");add("192.168.0.4");}}, IpWriter.write(new String[] {"192.168.0.1", "192.168.0.5"}));
 	}
 
 }
