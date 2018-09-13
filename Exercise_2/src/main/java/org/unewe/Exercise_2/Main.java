a6cc79f017e7ed7568535a0d7f8a41d4cd23cc1b
Marsianca <Marsianca@192.168.1.198>
2018-09-13 18:54:42 +0300

    little editing

--- Exercise_2/src/main/java/org/unewe/Exercise_2/Main.java
+++ Exercise_2/src/main/java/org/unewe/Exercise_2/Main.java
@@ -4,26 +4,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) {
-		
-		String fio;
-		if(args.length < 2) {
-			Scanner sc = new Scanner(System.in);
-			
-			System.out.print("Введите ФИО пользователя (формат: Иванов И.И.): ");
-			fio = sc.nextLine();
-		} else {
-			fio = args[0] + " " + args[1];
-		}
-		
 		PhoneBook phoneBook = new PhoneBookImpl();
-		ArrayList<String> list = phoneBook.getPhoneNumbersFrom(fio);
-		try {
+		
+		System.out.println("Введите ФИО формата: Иванов.И.И");
+		ArrayList<String> list = phoneBook.getPhoneNumbersFrom(
+								new Scanner(System.in).nextLine());
+	
+		if(list != null) {
 			for(int i = 0; i < list.size(); i++) {
 				System.out.println((i + 1) + ". " + list.get(i));
 			}
-		} catch(NullPointerException e) {
-			System.err.println("Error: Пользователь не найден");
+		} else {
+			System.err.println("Пользователь не найден.");
 		}
 	}
 }
--- Exercise_2/src/main/java/org/unewe/Exercise_2/PhoneBookImpl.java
+++ Exercise_2/src/main/java/org/unewe/Exercise_2/PhoneBookImpl.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PhoneBookImpl implements PhoneBook {
-	private HashMap<String , ArrayList<String>> book = new HashMap() {{
-		put("Иванов И.И.", new ArrayList() {
+	//Условная записная книжка
+	private HashMap<String , ArrayList<String>> book = new HashMap<String , ArrayList<String>>() {{
+		put("Иванов И.И.", new ArrayList<String>() {
 			{add("+79873457589"); add("+79801693412"); add("+79874436622");}});
-		put("Сидоров В.С.", new ArrayList() {
+		put("Сидоров В.С.", new ArrayList<String>() {
 			{add("+79874458217"); add("+79801257654"); add("+79875548682");}});
-		put("Петров Р.Н.", new ArrayList() {
+		put("Петров Р.Н.", new ArrayList<String>() {
 			{add("+79878467574"); add("+79801455654"); add("+79874474568");}});
 	}};
 	
@@ -17,9 +18,13 @@ public class PhoneBookImpl implements PhoneBook {
 		super();
 	}
 	
-
+	
 	@Override
-	public ArrayList<String> getPhoneNumbersFrom(String name) {	
-		return this.book.get(name);
+	public ArrayList<String> getPhoneNumbersFrom(String name) {
+		try {
+			return this.book.get(name);
+		} catch (NullPointerException e) {
+			return null;
+		}
 	}
 }
